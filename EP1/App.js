a13bcd84171c6641145bee9507dce0a26b783170
Kumar Himanshu <khimanshu0507@gmail.com>
2024-01-01 21:11:50 +0530

    Exercise Assignment EP-03

--- EP1/App.js
+++ EP1/App.js
@@ -38,4 +38,22 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(HeadingComponent()); // You can also call the function as at the end of the day all functional component is JS function
 
+//Exercise Assignmensts
+// const HeaderComponent = () =>{
+//     return(
+//         <div className='header-container'>
+//             <div className='logo'>
+//                 <img alt="logo" width={"20px"} height={"20px"} src='https://i.pinimg.com/originals/82/c6/5b/82c65b9bb0a75026fc4c82a438b4cc9b.jpg' />
+//             </div>
+//             <div className='search'>
+//                 <input type='text'></input>
+//                 <button type="button" onClick={()=> console.log("clicked")}>Search </button>
+//             </div>
+//             <div className='userIcon'>
+//                 <img alt="user-icon" width={"20px"} height={"20px"} src='https://cdn-icons-png.flaticon.com/512/666/666201.png'/>
+//             </div>
+//         </div>
+//     )
+// }
+
 root.render(<HeadingComponent />)
