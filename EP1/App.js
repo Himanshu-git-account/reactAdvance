0d9b8bcdc924ce13bfd5b25f5dccb11c77531015
Kumar Himanshu <khimanshu0507@gmail.com>
2024-01-02 02:39:03 +0530

    EP4- Talk is cheap

--- EP1/App.js
+++ EP1/App.js
@@ -1,59 +1,77 @@
 import React from 'react'
-import  ReactDOM  from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { restaurantList } from './restaurantData'
 
-const heading = React.createElement("h1",{id:"heading"},"This is React createElement");
+const Header = () => {
+    return (<div className='headerContainer'>
+        <img
+            width={"100px"}
+            height={"100px"}
+            src="https://img.freepik.com/premium-vector/food-delivery-logo-template-design-creative-vector-emblem-icon-design-concept_306040-854.jpg"></img>
+        <div className='navContainer'>
+            <ul className='navItems'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>)
+}
 
-console.log(heading)
+const Body = () => {
+    return (
+        <div className='bodyContainer'>
+            <SearchBar />
+            <div className='resContainer'>
+                {restaurantList.map((restaurant) => <RestaurantDetails key={restaurant.data.id} restaurantObj={restaurant} />)}
+            </div>
+        </div>
+    )
+}
 
-//JSX is not HTML inside JS , but its a HTML like syntax
-// jsxHeading  =>(Babel) =>React.createElement(object) => HTMLElement(browser understand)
+const RestaurantDetails = (props) => {
 
+    const { name, cuisines, avgRating, costForTwo, deliveryTime, cloudinaryImageId } = props?.restaurantObj?.data;
+    return (
+        <div className='resDetailsCard'>
+            <img className="imgContainer" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}></img>
+            <div className='resDetails'>
+                <h3>{name}</h3>
+                <h4>{cuisines.join(", ")}</h4>
+                <h4>{avgRating}</h4>
+                <h4>₹{costForTwo / 100} FOR TWO</h4>
+                <h4>{deliveryTime} minutes</h4>
+            </div>
+        </div>
+    )
+}
 
-const Title = () =>{
-    return <div >
-        This is Title
+const SearchBar = () => {
+    return (
+        <div className='searchcontainer'>
+            <input type='text'></input>
+            <button type='text'>Search</button>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return <div className='footerContainer'>
+        Copyright @2023
     </div>
 }
 
-// If the APi is passing any malicious code then the jSX automatically sanituze it before rendering. This prevents XSS-attacks
-const dataFromAPi = 1000;
-
-
-//Component Composition - Rendering one component inside another
-
-// All the three inside it are same
-const HeadingComponent = () => (
-    <div >
-        {dataFromAPi} 
-        <Title />
-        <Title></Title>
-        {Title()}
-        <h1>This is headoingh</h1>
-    </div>
-)
-
-const jsxHeading = <h1>This is React JSX</h1>
+const AppLayout = () => {
+    return (
+        <div className="appContainer">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(HeadingComponent()); // You can also call the function as at the end of the day all functional component is JS function
-
-//Exercise Assignmensts
-// const HeaderComponent = () =>{
-//     return(
-//         <div className='header-container'>
-//             <div className='logo'>
-//                 <img alt="logo" width={"20px"} height={"20px"} src='https://i.pinimg.com/originals/82/c6/5b/82c65b9bb0a75026fc4c82a438b4cc9b.jpg' />
-//             </div>
-//             <div className='search'>
-//                 <input type='text'></input>
-//                 <button type="button" onClick={()=> console.log("clicked")}>Search </button>
-//             </div>
-//             <div className='userIcon'>
-//                 <img alt="user-icon" width={"20px"} height={"20px"} src='https://cdn-icons-png.flaticon.com/512/666/666201.png'/>
-//             </div>
-//         </div>
-//     )
-// }
-
-root.render(<HeadingComponent />)
+root.render(<AppLayout />)
